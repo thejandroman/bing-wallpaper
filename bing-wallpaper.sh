4cd316630cc86a635de4eb244cbcd066fa299cc8
hotoo <hotoo.cn@gmail.com>
2013-01-06 15:02:58 +0800

    A: support www.bing.com and cn.bing.com by cookie

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,19 +4,32 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-#urls=( $(curl -s http://www.bing.com|grep -Eo "url:'.*?'"|sed -e "s/url:\'\(.*\)\'/http:\/\/bing.com\1/"|sed -e "s/\\\//g") )
-urls=( $(curl -s http://cn.bing.com|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
-
-for p in ${urls[@]}; do
-    filename=$(echo $p|sed -e "s/.*%2f\(.\)/\1/")
-    if [ ! -f $PICTURE_DIR/$filename ]; then
-        if [[ "$p" != http://* ]]; then
-            p="http://cn.bing.com$p"
-        fi
-        echo "Downloading: $filename ..."
-        #wget -q -O $PICTURE_DIR/$filename $p
-        curl -Lo "$PICTURE_DIR/$filename" $p
+function dlWallpaper(){
+    if [[ "$1" = "cn" ]]; then
+        cookieFile="bing-wallpaper-cookie-cn"
+        bingUrl="http://cn.bing.com"
+    elif [[ "$1" = "en" ]]; then
+        cookieFile="bing-wallpaper-cookie-en"
+        bingUrl="http://www.bing.com"
     else
-        echo "Skipping: $filename ."
+        return 1
     fi
-done
+
+    urls=( $(curl --cookie "$cookieFile" -s "$bingUrl"|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
+    for p in ${urls[@]}; do
+        filename=$(echo $p|sed -e "s/.*%2f\(.\)/\1/")
+        if [ ! -f $PICTURE_DIR/$filename ]; then
+            if [[ "$p" != http://* ]]; then
+                p="$bingUrl$p"
+            fi
+            echo "Downloading: $filename ..."
+            #wget -q -O $PICTURE_DIR/$filename $p
+            curl -Lo "$PICTURE_DIR/$filename" $p
+        else
+            echo "Skipping: $filename ."
+        fi
+    done
+}
+
+dlWallpaper en
+dlWallpaper cn
