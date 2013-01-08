c1945ce9fa1fadb51c89f1e29cb30d6aa64ab870
hotoo <hotoo.cn@gmail.com>
2013-01-08 10:08:19 +0800

    U: clear codes

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,25 +4,18 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-CURR_DIR=$(cd "$(dirname "$0")"; pwd)
+BING_URL_CN="http://cn.bing.com"
 COOKIE_CN="_FS=NU=1&mkt=zh-cn&hta=on&ui=#zh-cn"
+
+BING_URL_EN="http://www.bing.com"
 COOKIE_EN="_FS=NU=1&mkt=en-us&hta=on&ui=#en-us"
 
 
 function dlWallpaper(){
-    if [[ "$1" = "cn" ]]; then
-        #cookieFile="${CURR_DIR}/bing-wallpaper-cookie-cn"
-        cookie=$COOKIE_CN
-        bingUrl="http://cn.bing.com"
-    elif [[ "$1" = "en" ]]; then
-        #cookieFile="${CURR_DIR}/bing-wallpaper-cookie-en"
-        cookie=$COOKIE_EN
-        bingUrl="http://www.bing.com"
-    else
-        return 1
-    fi
+    bingUrl=$1
+    cookie=$2
 
-    urls=( $(curl --cookie "$cookie" -s "$bingUrl"|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
+    urls=( $(curl -b "$cookie" -s "$bingUrl"|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
     for p in ${urls[@]}; do
         filename=$(echo $p|sed -e "s/.*%2f\(.\)/\1/")
         if [ ! -f $PICTURE_DIR/$filename ]; then
@@ -38,5 +31,5 @@ function dlWallpaper(){
     done
 }
 
-dlWallpaper en
-dlWallpaper cn
+dlWallpaper $BING_URL_EN $COOKIE_EN
+dlWallpaper $BING_URL_CN $COOKIE_CN
