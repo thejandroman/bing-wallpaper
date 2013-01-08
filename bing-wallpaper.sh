4a539aed1ea224608112aa71c828e3414414c458
hotoo <hotoo.cn@gmail.com>
2013-01-08 09:38:01 +0800

    U: cookie by string, without cookie file

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,18 +4,25 @@ PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
+CURR_DIR=$(cd "$(dirname "$0")"; pwd)
+COOKIE_CN="_FS=NU=1&mkt=zh-cn&hta=on&ui=#zh-cn"
+COOKIE_EN="_FS=NU=1&mkt=en-us&hta=on&ui=#en-us"
+
+
 function dlWallpaper(){
     if [[ "$1" = "cn" ]]; then
-        cookieFile="bing-wallpaper-cookie-cn"
+        #cookieFile="${CURR_DIR}/bing-wallpaper-cookie-cn"
+        cookie=$COOKIE_CN
         bingUrl="http://cn.bing.com"
     elif [[ "$1" = "en" ]]; then
-        cookieFile="bing-wallpaper-cookie-en"
+        #cookieFile="${CURR_DIR}/bing-wallpaper-cookie-en"
+        cookie=$COOKIE_EN
         bingUrl="http://www.bing.com"
     else
         return 1
     fi
 
-    urls=( $(curl --cookie "$cookieFile" -s "$bingUrl"|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
+    urls=( $(curl --cookie "$cookie" -s "$bingUrl"|grep -Eo "url:'[^']*'"|sed -e "s/url:\'\([^']*\)\'/\1/"|sed -e "s/\\\//g") )
     for p in ${urls[@]}; do
         filename=$(echo $p|sed -e "s/.*%2f\(.\)/\1/")
         if [ ! -f $PICTURE_DIR/$filename ]; then
